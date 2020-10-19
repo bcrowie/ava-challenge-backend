cbcd0643056a63943fbb70c0ec9e4c9385963963
Brenden Crowie <brendencrowie@gmail.com>
2020-10-18 22:25:19 -0400

    Frontend url

--- src/models/infoModel.js
+++ src/models/infoModel.js
@@ -5,13 +5,16 @@ module.exports = {
     name: "Brenden Crowie",
   },
   frontend: {
-    url: "frontend url",
+    url: "https://ava-challenge-frontend.herokuapp.com/",
   },
   language: "node.js",
-  sources: "github repo",
+  sources: {
+    frontend: "https://github.com/bcrowie/ava-challenge-frontend.git",
+    backend: "https://github.com/bcrowie/ava-challenge-backend.git",
+  },
   answers: {
-    1: "",
-    2: "",
-    3: "",
+    1: "While I was solving the algorithm I compared the index of the mutation being made to the index of the last mutation. After determining if the new mutation index was higher or lower I added or subtracted the length of the deletions or insertions as needed.",
+    2: "I would have completed the front end.",
+    3: "The only change I could see being made is an adjustment to the challenge description. Some of the information is either wrong (possibly a typo), or there are grammar conflicts.",
   },
 };
